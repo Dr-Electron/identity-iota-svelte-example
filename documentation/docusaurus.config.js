c13e8d8868be2cdb3bf899e6ca384e82cb91dd11
Jeroen van den Hout <jlvandenhout@gmail.com>
2022-04-19 00:11:16 +0200

    Remove unused entries

--- documentation/docusaurus.config.js
+++ documentation/docusaurus.config.js
@@ -1,33 +1,30 @@
 const path = require('path');
 
 module.exports = {
-    title: 'IOTA Client WASM Tutorial',
-    url: '/',
-    baseUrl: '/',
-    themes: ['@docusaurus/theme-classic'],
-    plugins: [
-        [
-            '@iota-wiki/plugin-tutorial',
-            {
-                title: 'IOTA Client WASM Tutorial',
-                description:
-                    'This tutorial shows you how to use the WASM IOTA client',
-                preview: 'iota_client.png',
-                route: 'wasm-client-tutorial/tutorial',
-                source: 'https://github.com/Dr-Electron/identity-iota-svelte-example/tree/gitpod-integration',
-                tags: ['text', 'gettingstarted', 'wasm', 'client'],
-            },
-        ],
-        [
-            '@docusaurus/plugin-content-docs',
-            {
-                id: 'wasm-client-tutorial',
-                path: path.resolve(__dirname, './docs'),
-                routeBasePath: 'wasm-client-tutorial',
-                sidebarPath: path.resolve(__dirname, './sidebars.js'),
-                editUrl: 'https://github.com/Dr-Electron/identity-iota-svelte-example/edit/gitpod-integration/',
-            },
-        ],
+  plugins: [
+    [
+      '@iota-wiki/plugin-tutorial',
+      {
+        title: 'IOTA Client WASM Tutorial',
+        description: 'This tutorial shows you how to use the WASM IOTA client',
+        preview: 'iota_client.png',
+        route: 'wasm-client-tutorial/tutorial',
+        source:
+          'https://github.com/Dr-Electron/identity-iota-svelte-example/tree/gitpod-integration',
+        tags: ['text', 'gettingstarted', 'wasm', 'client'],
+      },
     ],
-    staticDirectories: [path.resolve(__dirname, './images')],
+    [
+      '@docusaurus/plugin-content-docs',
+      {
+        id: 'wasm-client-tutorial',
+        path: path.resolve(__dirname, './docs'),
+        routeBasePath: 'wasm-client-tutorial',
+        sidebarPath: path.resolve(__dirname, './sidebars.js'),
+        editUrl:
+          'https://github.com/Dr-Electron/identity-iota-svelte-example/edit/gitpod-integration/',
+      },
+    ],
+  ],
+  staticDirectories: [path.resolve(__dirname, './images')],
 };
